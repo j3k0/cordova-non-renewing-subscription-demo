26206c130839308da2daa361fa19d94fe105ec0d
Jean-Christophe Hoelt <hoelt@fovea.cc>
2016-05-15 21:45:08 +0300

    Working prototype on iOS

--- cordova-app/www/js/cordova-non-renewing-subscription.js
+++ cordova-app/www/js/cordova-non-renewing-subscription.js
@@ -5,6 +5,20 @@
   // Define and export nonRenewing
   var nonRenewing = window.nonRenewing = {};
 
+  var log = function() {
+    var args = Array.prototype.concat.apply([ "[nonRenewing]" ], arguments);
+    console.log.apply(console.log, args);
+  };
+
+  // Create prefixed className from list of names, or a single names.
+  //
+  // examples:
+  // ---------
+  // className('bob')
+  //   -> 'cordova-non-renewing-bob'
+  // className(['alice', 'bbob'])
+  //   -> 'cordova-non-renewing-alice cordova-non-renewing-bob'
+  //
   var className = function(names) {
     if (typeof names == 'string')
       return 'cordova-non-renewing-' + names;
@@ -27,7 +41,8 @@
     });
   }
 
-  var View = function(parent) {
+  var View = function(parent, eventsHandler) {
+      this.eventsHandler = eventsHandler || function(){};
       this.el = document.createElement('div');
       this.el.className = className('root');
       this.el.style.position = 'absolute';
@@ -52,68 +67,298 @@
     this.el.innerHTML = html || '';
   };
 
+  var IOS_LIGHT_BLUE = '#54c7fc';
+  var IOS_DARK_BLUE  = '#0076ff';
+
   var Templates = {
-    title: function(content, extraClass) {
-      var cname = extraClass ? className("title", extraClass) : className("title");
-      return '<h2 class="' + cname + '">' + content + '</h2>';
+
+    el: function(type, baseClass, content, extraClass) {
+      var cname = extraClass ? className(baseClass, extraClass) : className("title");
+      return '<' + type + ' class="' + cname + '">' + content + '</' + type + '>';
     },
 
-    message: function(content, extraClass) {
+    title: function(content, extraClass) {
+      return this.el('h3', 'title', content, extraClass);
+    },
+
+    subtitle: function(content, extraClass) {
+      return this.el('h5', 'subtitle', content, extraClass);
+    },
+
+    message: function(content, extraClass, extraStyle) {
       var cname = extraClass ? className("message", extraClass) : className("message");
-      return '<p class="' + cname + '">' + content + '</p>';
+      var style = 'font-size:0.9em;';
+      if (extraStyle)
+        style += extraStyle;
+      return '<p style="' + style + '" class="' + cname + '">' + content + '</p>';
+    },
+
+    button: function(label, cname, extraStyle) {
+      var style = 'text-decoration:none;color:' + IOS_DARK_BLUE + ';';
+      if (extraStyle)
+        style += extraStyle;
+      return '<p class="' + className(cname) + '"><a style="' + style + '" href="#" class="' + className(cname + '-link') + '">' + label + '</a></p>';
+    },
+
+    cancelButton: function() {
+      return this.button('Cancel', 'cancel');
     },
 
     closeButton: function() {
-      var style =
-        'text-decoration:none;color:#54c7fc';
-      return '<p><a style="' + style + '" href="#" class="' + className('close') + '">Close</a></p>';
+      return this.button('Close', 'close');
     },
 
     dialogBox: function(innerHTML) {
       var innerStyle =
-         'position:absolute;' +
-         'padding:1em;' +
-         'left:2em; right:2em; top:5em; bottom:5em;' +
-         'background-color:white;' +
-         'border-radius:1em;' +
-         'text-align:center';
-      return '<div style="' + innerStyle + '">' + innerHTML + '</div>';
+        'position:absolute;' +
+        'padding:1em;' +
+        'top:50%;' +
+        'left:50%;' +
+        'transform:translateX(-50%) translateY(-50%);' +
+        '-webkit-transform:translateX(-50%) translateY(-50%);' +
+        'background-color:white;' +
+        'border-radius:0.7em;' +
+        'min-width: 14.5em;' +
+        'text-align:center';
+      return '<div class="' + className('dialog-box') + '" style="' + innerStyle + '">' + innerHTML + '</div>';
     },
 
     errorScreen: function(message) {
-      var content = Templates.message(escapeHtml(message), 'error');
-      return Templates.dialogBox(content + Templates.closeButton());
+      var title = this.title('Error');
+      var content = this.message(escapeHtml(message), 'error');
+      return this.dialogBox(title + content + this.closeButton());
+    },
+
+    loadingScreen: function(message) {
+      return this.dialogBox(this.message('Loading...', 'loading'));
+    },
+
+    statusScreen: function(data) {
+      var title = this.title('Subscription Status');
+      var content = '';
+      if (data.subscriber) {
+        content += this.message('Subscribed', null, 'font-weight:bold;');
+        content += this.message('Expiry Date:<br/>' + data.expiryDate);
+        content += this.button('Extend your subscription', 'renew', 'font-weight:bold;');
+      }
+      else if (data.expired) {
+        content += this.message('Subscription Expired', null, 'font-weight:bold');
+        content += this.message('Expiry Date:<br/>' + data.expiryDate);
+        content += this.button('Renew your subscription', 'renew', 'font-weight:bold;');
+      }
+      else {
+        content += this.message('Not Subscribed', null, 'font-weight:bold');
+        content += this.button('Subscribe now!', 'renew', 'font-weight:bold;');
+      }
+      return this.dialogBox(title + content + this.closeButton());
+    },
+
+    productsScreen: function(products) {
+      var title = this.title('Subscription Products');
+      var content = '';
+      for (var i = 0; i < products.length; ++i) {
+        content += this.subtitle(escapeHtml(products[i].title));
+        content += this.message(escapeHtml(products[i].description));
+        content += this.button(products[i].price, 'buy-' + i, 'font-weight:bold');
+      }
+      return this.dialogBox(title + content + this.cancelButton());
     }
   };
 
   View.prototype.bindEvents = function() {
-    var that = this;
-    document.getElementsByClassName(className('close'))[0].addEventListener('click', function(event) {
-      event.preventDefault();
-      that.hide();
-    });
+
+    var bindEvent = function(cname, event, fn) {
+      var el = document.getElementsByClassName(className(cname));
+      if (!el || el.length <= 0)
+        return;
+      el[0].addEventListener(event, fn.bind(this));
+    }.bind(this);
+
+    var bindButtonClick = function(cname) {
+      bindEvent(cname + '-link', 'click', function(event) {
+        event.preventDefault();
+        this.eventsHandler(cname);
+      });
+    };
+
+    var buttons = ['cancel', 'close', 'renew'];
+    for (var i = 0; i <= 9; ++i)
+      buttons.push('buy-' + i);
+    buttons.forEach(bindButtonClick);
   };
 
   View.prototype.showError = function(err) {
+    this.name = 'error';
     this.html(Templates.errorScreen(err));
     this.bindEvents();
     this.show();
   };
 
+  View.prototype.showLoading = function() {
+    this.name = 'loading';
+    this.html(Templates.loadingScreen());
+    this.show();
+  };
+
+  View.prototype.showStatus = function(data) {
+    this.name = 'status';
+    this.html(Templates.statusScreen(data));
+    this.bindEvents();
+    this.show();
+  };
+
+  View.prototype.showProducts = function(products) {
+    this.name = 'products';
+    this.html(Templates.productsScreen(products));
+    this.bindEvents();
+    this.show();
+  };
+
   var Controller = function(options) {
-    store.verbosity = options.verbosity || store.DEBUG;
+    this.view = options.view;
+    window.store.verbosity = options.verbosity || window.store.INFO;
     this.expiryStore = new ExpiryStore(options.loadExpiryDate, options.saveExpiryDate);
     this.registerProducts(options.products);
+    this.registerHandlers();
+    window.store.refresh();
   };
 
   Controller.prototype.registerProducts = function(products) {
+    this.productsById = {};
+    this.products = products;
     for (var i = 0; i < products.length; ++i) {
-      store.register({
+      window.store.register({
         id: products[i].id,
-        type: store.NON_RENEWING_SUBSCRIPTION,
+        type: window.store.NON_RENEWING_SUBSCRIPTION,
         duration: products[i].duration
       });
+      this.productsById[products[i].id] = products[i];
     }
+  };
+
+  Controller.prototype.registerHandlers = function() {
+
+    window.store.when("product").updated(function(p) {
+      log("Product updated: " + p.id + " (" + p.state + ")");
+
+      // Warn about invalid products
+      if (p.state == window.store.INVALID) {
+        log("Product " + p.id + " can't be loaded from the store.");
+        // this.products[p.id];
+      }
+
+      if (p.state == window.store.APPROVED) {
+        this.deliverOrder(p);
+      }
+    }.bind(this));
+
+    window.store.error(function(err) {
+      this.view.showError(err.message);
+    }.bind(this));
+
+    window.store.when("product").cancelled(function(p) {
+      this.view.hide();
+    }.bind(this));
+  };
+
+  Controller.prototype.deliverOrder = function(p) {
+    var nrProduct = this.productsById[p.id];
+    if (!nrProduct) {
+      // Unknown product type. Cleaning up.
+      p.finish();
+      return;
+    }
+
+    this.expiryStore.load(function(err, expiryDate) {
+      if (err) {
+        return this.view.showError("Can't process subscription: " + err);
+      }
+      if (!expiryDate)
+        expiryDate = +new Date();
+      expiryDate = expiryDate + nrProduct.duration * 1000;
+
+      this.expiryStore.save(expiryDate, function(err) {
+        if (err)
+          return this.view.showError("Can't process subscription: " + err);
+
+        log("finishing " + p.id);
+        p.finish();
+        return this.openStatusView();
+      }.bind(this));
+    }.bind(this));
+  };
+
+  Controller.prototype.loadStatus = function(cb) {
+    this.expiryStore.load(function(err, expiryDate) {
+      if (expiryDate > 0) {
+        var now = +new Date();
+        cb(null, {
+          expiryDate: new Date(expiryDate).toLocaleDateString() + " at " + new Date(expiryDate).toLocaleTimeString(),
+          subscriber: now <= expiryDate,
+          expired:    now > expiryDate
+        });
+      }
+      else {
+        cb(null, {
+          expiryDate: null,
+          subscriber: false,
+          expired:    false
+        });
+      }
+    });
+  };
+
+  Controller.prototype.loadProducts = function(cb) {
+    cb(null, this.products.map(function(p) {
+      return window.store.get(p.id);
+    }));
+  };
+
+  Controller.prototype.openStatusView = function() {
+    this.view.showLoading();
+    this.loadStatus(function(err, status) {
+      if (err)
+        return this.view.showError(err);
+      this.view.showStatus(status);
+    }.bind(this));
+  };
+
+  Controller.prototype.openProductsView = function() {
+    this.view.showLoading();
+    this.loadProducts(function(err, products) {
+      if (err)
+        return this.view.showError(err);
+      this.view.showProducts(products);
+    }.bind(this));
+  };
+
+  Controller.prototype.purchase = function(id) {
+    this.view.showLoading();
+    window.store.order(id);
+  };
+
+  Controller.prototype.eventsHandler = function(eventName) {
+
+    log("Event " + eventName);
+    if (eventName == 'close') {
+      this.view.hide();
+    }
+
+    if (eventName == 'cancel') {
+      if (this.view.name == 'products')
+        this.openStatusView();
+    }
+
+    if (eventName == 'renew') {
+      this.openProductsView();
+    }
+
+    if (eventName.slice(0,4) == 'buy-') {
+      var index = +eventName.slice(4);
+      log("Purchase product at index " + index);
+      this.purchase(this.products[index].id);
+    }
+
   };
 
   // The subscription status needs to be stored on a server, linked to a user.
@@ -123,15 +368,39 @@
   //   - saveExpiryDate(expiryDate, function(err) {})
   //   - loadExpiryDate(function(err, expiryDate) { ... })
   var ExpiryStore = function(load, save) {
-    this.load = load;
-    this.save = save;
+
+    load = load || function(cb) {
+      cb(null, localStorage.cordovanonrsdate);
+    };
+
+    save = save || function(value, cb) {
+      localStorage.cordovanonrsdate = value;
+      cb(null);
+    };
+
+    // Create a version of 'load' that makes sure expiryDate is a number.
+    // And caches the loaded value.
+    this.load = function(cb) {
+      if (this.expiryDate)
+        return cb(null, this.expiryDate);
+      load(function(err, expiryDate) {
+        this.expiryDate = +expiryDate;
+        cb(err, +expiryDate);
+      }.bind(this));
+    }.bind(this);
+
+    // Save, update the cache.
+    this.save = function(value, cb) {
+      this.expiryDate = +value;
+      save(value, cb);
+    }.bind(this);
   };
 
   nonRenewing.initialize = function(options) {
 
     this.view = new View(document.body);
 
-    if (true || !window.store) {
+    if (!window.store) {
       this.view.showError('The in-app purchase plugin is not available.');
       return;
     }
@@ -141,17 +410,12 @@
       products:  options.products,
       view:      this.view
     });
+
+    this.view.eventsHandler = this.controller.eventsHandler.bind(this.controller);
   };
 
-  nonRenewing.manage = function(options) {
-    this.view.showLoading();
-    this.controller.loadStatus(function(err, status) {
-      if (err) {
-        alert("ERROR: " + err);
-        return;
-      }
-      this.view.showStatus(status);
-    });
+  nonRenewing.openSubscriptionManager = function() {
+    this.controller.openStatusView();
   };
 
   /*
--- cordova-app/www/js/index.js
+++ cordova-app/www/js/index.js
@@ -1,15 +1,36 @@
-var app = {
-    initialize: function() {
-        console.log('listening to deviceready');
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
+var app = {};
 
-    onDeviceReady: function() {
-        console.log('initializing the nonRenewing plugin');
-        nonRenewing.initialize({
-            products: []
-        });
-    }
+app.initialize = function() {
+    console.log('listening to deviceready');
+    document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+};
+
+app.onDeviceReady = function() {
+    console.log('initializing the nonRenewing plugin');
+    /* store.register({
+        type: store.PAID_SUBSCRIPTION,
+        id: 'cc.fovea.purchase.subscription1'
+    }); */
+    nonRenewing.initialize({
+        verbosity: store.DEBUG,
+        products: [{
+            id: 'cc.fovea.purchase.nonrenewing.1hour',
+            duration: 3600
+        }, {
+            id: 'cc.fovea.purchase.nonrenewing.5minutes',
+            duration: 300
+        }]
+    });
+    this.showMainScreen();
+};
+
+app.showMainScreen = function() {
+    document.getElementsByClassName('app')[0].innerHTML = '<h1>Demo</h1><p><a href="#" class="manage-subscription">Manage your subscription</a></p>';
+    document.getElementsByClassName('manage-subscription')[0].addEventListener('click', function(event) {
+        console.log('showMainScreen -> openSubscriptionManager');
+        event.preventDefault();
+        nonRenewing.openSubscriptionManager();
+    });
 };
 
 app.initialize();
